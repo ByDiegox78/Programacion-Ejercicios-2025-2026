ef3b7bcf18cb32ed194d59c843c010dc4465230a
ByDiegox78 <oms04081974@gmail.com>
2025-12-18 12:06:08 +0100

    Añadido Star wars actuializado

---
+++ Prog.Objetos/StarWars/StarWars/Factory/DroidFactory.cs
@@ -0,0 +1,14 @@
+﻿using StarWars.Models;
+
+namespace StarWars.Factory;
+
+public static class DroidFactory {
+    public static Droide RandDroid() {
+        var random = new Random().Next(1, 101);
+        return random switch {
+            <= 30 => new Droide(50, Droide.TipoDroide.Sw348, new Random().Next(9, 13), 0, 0),
+            <= 80 => new Droide(100, Droide.TipoDroide.Sw447, 0, new Random().Next(9, 13), 0),
+            _ => new Droide(new Random().Next(100, 151), Droide.TipoDroide.Sw421, 0, 0, new Random().Next(10, 31))
+        };
+    }
+}
---
+++ Prog.Objetos/StarWars/StarWars/Program.cs
@@ -0,0 +1,15 @@
+﻿using StarWars.Config;
+using StarWars.Service;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Main();
+
+void Main() {
+    Configuration.ParseArguments(args);
+    var servicio = new DroidService(
+        Configuration.MapSize,
+        Configuration.NumberOfEnemies,
+        Configuration.Time
+    );
+    servicio.Simulation();
+}
