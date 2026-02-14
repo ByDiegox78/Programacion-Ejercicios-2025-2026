c2d549a0c8013636d15ba9e320fd147c0222e38b
ByDiegox78 <oms04081974@gmail.com>
2026-02-14 11:33:55 +0100

    Asesinos Validator

--- Prog.Genericos/Lol/Lol/Validator/AsesinoValidate.cs
+++ Prog.Genericos/Lol/Lol/Validator/AsesinoValidate.cs
@@ -7,23 +7,44 @@ namespace Lol.Validator;
 public class AsesinoValidate : IValidador<Campeon> {
     private static readonly decimal[] PreciosValidos = { 450, 1350, 3150, 4800, 6300, 7800, 4444, 3141 };
     public IEnumerable<string> Validar(Campeon campeon) {
-        var errores = new Lista<string>();
+        var errores = new List<string>();
         
 
         if (campeon is not Asesino asesino) {
-            errores.AgregarFinal("El campeon no es un asesino");
+            errores.Add("El campeon no es un asesino");
+            return errores;
         }
         
         if (string.IsNullOrWhiteSpace(campeon.Nombre) || campeon.Nombre.Length < 2)
-            errores.AgregarFinal("El nombre del docente es obligatorio (mín. 2 car.).");
+            errores.Add("El nombre del campeon es obligatorio (mín. 2 car.).");
 
         if (campeon.Nivel is < 0 or > 18) {
-            errores.AgregarFinal("El nivel no puede ser menor que 0 o mayor que 18.");
+            errores.Add("El nivel no puede ser menor que 0 o mayor que 18.");
         }
 
         if (campeon.PrecioEsencias is not (450 or 1350 or 3150 or 4800 or 6300 or 7800)) {
-            errores.AgregarFinal("El precio intricucido es valido; Precios validos: 450, 1350, 3150, 4800, 6300, 7800, 4444, 3141");
+            errores.Add("El precio intricucido es valido; Precios validos: 450, 1350, 3150, 4800, 6300, 7800, 4444, 3141");
         }
-        
+
+        if (asesino.Letalidad is < 0 or > 100) {
+            errores.Add("La letalidad introducida no puede ser negativa");
+        }
+
+        if (campeon.HabilidadCampeon.Count != 4) {
+            errores.Add("El campeón debe tener exactamente 4 habilidades (Q, W, E, R)");
+        }
+
+        foreach (var habilidad in campeon.HabilidadCampeon) {
+            if (string.IsNullOrWhiteSpace(habilidad.Nombre) || habilidad.Nombre.Length < 2)
+                errores.Add("El nombre del docente es obligatorio (mín. 2 car.).");
+            if (habilidad.Cooldawn is < 0 ) {
+                errores.Add("El cooldawn no puede ser negativo");
+            }
+            
+        }
+
+
+        return errores;
+
     }
 }
